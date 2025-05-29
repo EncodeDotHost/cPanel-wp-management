b7aa5221a4ffff41a7c728eba7a3768f128d5593
Neil Batchelor <nb@encode.host>
2025-05-29 12:27:31 +0100

    Created wp-cron-run.sh

---
+++ wp-cron-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+. "$(dirname "$0")"/acct-common.sh
+for i in $accounts
+do
+        . "$(dirname "$0")"/acct-ind.sh
+        echo "***User $i / $domain ***";
+        su - $i -c "wp cron event run --due-now --path=$path"
+        echo "";
+done 
