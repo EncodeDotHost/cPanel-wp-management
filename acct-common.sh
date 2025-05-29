ee3e44ee0fa1244c449c439830c852f5324896bb
Neil Batchelor <nb@encode.host>
2025-05-29 15:14:30 +0100

    Exclude both domain and account in acc-common.sh

--- acct-common.sh
+++ acct-common.sh
@@ -2,6 +2,10 @@
 # Get all domains and their document roots
 accounts=$(for user in $(cat /etc/userdomains | awk '{print $2}' | sort | uniq); do
     if [ -f "/var/cpanel/userdata/$user/main" ]; then
-        echo "$user:$(grep "^documentroot:" "/var/cpanel/userdata/$user/main" | awk '{print $2}')"
+        domain=$(grep "^domain:" "/var/cpanel/userdata/$user/main" | awk '{print $2}')
+        # Skip if either username or domain is in nonwp
+        if ! grep -qE "^$user$|^$domain$" "$(dirname "$0")/nonwp"; then
+            echo "$user:$(grep "^documentroot:" "/var/cpanel/userdata/$user/main" | awk '{print $2}')"
+        fi
     fi
-done | grep -vE "($(cat "$(dirname "$0")"/nonwp | tr '\n' '|' | sed 's/|$//'))")
+done)
